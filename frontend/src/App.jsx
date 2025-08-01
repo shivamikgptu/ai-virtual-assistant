27e1f43f821240c85e2f73eeeef99db48475b5b6
Shivam Yadav <shivamptu4566@gmail.com>
2025-08-01 10:48:13 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import SignUp from './pages/SignUp.jsx'
 import SignIn from './pages/SignIn.jsx'
 import Customize from './pages/Customize.jsx'
 import Customize2 from './pages/Customize2.jsx'
-import { userDataContext } from './context/userContext.jsx';
+import { userDataContext } from './context/UserContext.jsx';
 import Home from './pages/Home.jsx';
 
 
